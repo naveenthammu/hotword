51a8d905c8c5a4ced49fd0bdf2411215fdb3b5d8
naveen Rv <46121222+naveenthammu@users.noreply.github.com>
2021-01-15 20:04:54 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+import Snowboy.snowboydecoder as snowboydecoder
+def detected_callback():
+    print ("hotword detected")
+detector = snowboydecoder.HotwordDetector("Hey_edith.pmdl", sensitivity=0.5, audio_gain=1)
+detector.start(detected_callback)
